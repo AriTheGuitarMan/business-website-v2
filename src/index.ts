e45b80f853fb336c656e55182ba2840cb106dc29
FIRST_NAME LAST_NAME <15020608+AriTheGuitarMan@users.noreply.github.com>
2025-07-21 21:33:44 -0400

    x

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { WorkerEntrypoint } from "cloudflare:workers";
+import { WorkerEntrypoint } from "@cloudflare:workers";
 
 export default class extends WorkerEntrypoint<Env> {
-  async fetch(request: Request) {
+  async fetch(request: Request, env, ctx) {
     // You can perform checks before fetching assets
     const user = await checkIfRequestIsAuthenticated(request);
 
@@ -11,6 +11,9 @@ export default class extends WorkerEntrypoint<Env> {
 
     // You can then just fetch the assets as normal, or you could pass in a custom Request object here if you wanted to fetch some other specific asset
     const assetResponse = await this.env.ASSETS.fetch(request);
+
+   const url = "https://waldfamilyoffice.com";
+   const modifiedRequest = new Request(url, request);
 
     // You can return static asset response as-is, or you can transform them with something like HTMLRewriter
     return new HTMLRewriter()
